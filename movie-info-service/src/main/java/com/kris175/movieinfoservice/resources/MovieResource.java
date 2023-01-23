ad2c65ec5cfbfb894df6d626fcab247e4882ba3b
kris175 <harikris175@gmail.com>
2023-01-23 11:02:06 +1100

    Added controllers for info and catalog

---
+++ movie-info-service/src/main/java/com/kris175/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,15 @@
+package com.kris175.movieinfoservice.resources;
+
+import com.kris175.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId, "Test name", "Test desc");
+    }
+}
